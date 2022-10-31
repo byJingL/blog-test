19f616ee83a0f599666b3d38ee1215a438e7477b
Jing <jing.lu.cn@outlook.com>
2022-10-31 12:46:00 +0000

    update SQLite to PostgreSQL

--- main.py
+++ main.py
@@ -10,14 +10,15 @@ from functools import wraps
 # relationship database
 from sqlalchemy import Table, Column, Integer, ForeignKey
 from sqlalchemy.orm import relationship
-#
+# using of avatar photo
 from flask_gravatar import Gravatar
+import os
 
 today = datetime.datetime.now().strftime("%B %d,%Y")
 
 # ------------------ Create APP------------------#
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 ckeditor = CKEditor(app)
 Bootstrap(app)
 login_manager = LoginManager()
@@ -33,7 +34,7 @@ def load_user(user_id):
 
 
 # ------------------ Create DB------------------#
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
